be268083bb6f596e58e37521edffd96e5c108912
OscarUrielCZ <oscar2410.oucz@gmail.com>
2020-08-14 01:06:51 -0500

    Algunas páginas hechas

---
+++ header.php
@@ -0,0 +1,17 @@
+<header class="menubar">
+	<nav>
+		<ul>
+			<li class="active"><a href="index.php">Inicio</a></li>
+			<li>
+				<a href="#">Básicos</a>
+				<div class="submenu1">
+					<ul>
+						<li><a href="syntax.php">Sintaxis</a></li>
+						<li><a href="superglobals.php">Variables superglobales</a></li>
+					</ul>
+				</div>
+			</li>
+			<li><a href="people.php">Personas</a></li>
+		</ul>
+	</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/menubar.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<title>Aprendiendo PHP | Inicio</title>
+</head>
+<body>
+	<?php
+		require_once 'header.php';
+	?>
+	<div class="container">
+		<div id="title">
+			<h2>Aprendiendo PHP</h2>
+			<p>En esta paǵina hay información sobre lo aprendido con PHP en el <a href="https://www.udemy.com/course/master-en-php-sql-poo-mvc-laravel-symfony-4-wordpress/">curso de PHP</a></p>
+		</div>
+	</div>
+</body>
+</html>
